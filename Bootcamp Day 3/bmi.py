ec0e00eca83323508e01366324583870b1673364
Moraes97-Murilo <murilo.alves8@gmail.com>
2021-04-20 07:35:18 -0300

    bmi coding

--- Bootcamp Day 3/bmi.py
+++ Bootcamp Day 3/bmi.py
@@ -1,1 +1,13 @@
-#calculating
+#BMI calculator
+
+height = float(input("Hi, how tall are you? "))
+weight = float(input("What's your weight? "))
+
+bmi = round(weight/height**2, 2)
+
+if bmi < 18:
+    print(f"You are underweighted, your bmi is {bmi}.")
+elif 18 < bmi < 26:
+    print(f"You are regular, your bmi is {bmi}.")
+else:
+    print(f"You are overweighted, your bmi is {bmi}.")
