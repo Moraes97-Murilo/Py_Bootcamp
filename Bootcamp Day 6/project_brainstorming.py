2c5ffdbdee31bf7291774bd3cd334513b117e867
Murilo Moraes <66094955+Moraes97-Murilo@users.noreply.github.com>
2021-04-25 01:54:10 -0300

    Create project_brainstorming.py

---
+++ Bootcamp Day 6/project_brainstorming.py
@@ -0,0 +1,11 @@
+# In this file, I will just write some of my ideas, because in the day 6, teacher demanded solve tasks at Reeborg page
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
+
+'''
+Ideas:
+
+1 -
+2 -
+3 -
+
+'''
