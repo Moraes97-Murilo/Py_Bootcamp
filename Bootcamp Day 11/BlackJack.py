1b36862477c16aab66e07421821d86ff8c635176
Moraes97-Murilo <murilo.alves8@gmail.com>
2021-07-18 01:08:32 -0300

    cards

--- Bootcamp Day 11/BlackJack.py
+++ Bootcamp Day 11/BlackJack.py
@@ -1,2 +1,18 @@
 #Now this bootcamp is getting interesting
 #18/07/2021
+import random
+
+def card_dealer():
+    '''Returns a random card from the deck.'''
+    cards = [11,2,3,4,5,6,7,8,9,10]
+    c = random.choice(cards)
+    return c
+
+player_set, pc_set = [], []
+
+for i in range(4):
+    dealer_outp = card_dealer()
+    if i%2 == 0:
+        player_set.append(dealer_outp)
+    else:
+        pc_set.append(dealer_outp)
