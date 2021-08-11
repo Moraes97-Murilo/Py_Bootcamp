f3984524ef8e715099a1422da357f12eaaff89c6
Moraes97-Murilo <murilo.alves8@gmail.com>
2021-08-11 00:18:33 -0300

    final crazy deploy

--- Bootcamp Day 12/Guess.py
+++ Bootcamp Day 12/Guess.py
@@ -4,17 +4,17 @@ import time
 
 def check_guess(randnum,gnum):
     if gnum ==  randnum:
-        print("Finally! You're a crazy one, like me... and...")
+        print("\nFinally! You're a crazy one, like me... and...")
         time.sleep(1.5)
         print('You win!!!\n\n\n')
         return True
     elif gnum > randnum:
-        print("Stop being a silly, you must be CRAZY to win this game... and...")
+        print("\nStop being a silly, you must be CRAZY to win this game... and...")
         time.sleep(1.8)
         print('The number is lower than your guess! You silly!')
         return False
     else:
-        print("Stop being a fool, you must be CRAZY to win this game... and...")
+        print("\nStop being a fool, you must be CRAZY to win this game... and...")
         time.sleep(1.8)
         print('The number is higher than your guess! You fool!')
         return False
@@ -34,19 +34,23 @@ while game:
         turns = 3
     else:
         print('You type something wrong, game is restarting...')
+    
     try:
         for i in range(0,turns):
             guess = int(input(f'{i+1}°try - Guess a number between 1 and 100: '))
             check = check_guess(num,guess)
-            if check == False:
+            if i == (turns-1):
+                time.sleep(1)
+                print(chr(27) + "[2J")
+                print("Your attempts are over, you're NOT CRAZY just IDIOT!!")
+                time.sleep(7)
+            elif check == False:
                 pass
             else:
                 again = input('\nDo you wanna play again? \na)yes\nb)no\n---> ')
-                if again == "b" or again == "no":
+                if again == "b" or again == "no" or again == "n":
                     game = False
                 break
-        print("Your attempts are over, you're NOT CRAZY just IDIOT!!")
-        time.sleep(3)
         print(chr(27) + "[2J")
     except:
         print(chr(27) + "[2J")
