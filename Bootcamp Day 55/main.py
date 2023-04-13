fcd5bc049e6a2fb03c3139e2e032954381884865
Murilo Moraes <murilo.alves8@gmail.com>
2023-04-13 01:13:05 -0300

    changes on defs

--- Bootcamp Day 55/main.py
+++ Bootcamp Day 55/main.py
@@ -3,11 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/username")
-def hello_world():
+def user_home():
     return "Hi, this page works to greet new users."
 
 @app.route("/username/<name>")
-def kanpai(name):
+def greet_user(name):
     return f"Hello, {name}, nice to meet you!"
 
 if __name__ == "__main__":
